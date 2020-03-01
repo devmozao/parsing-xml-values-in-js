d73f3928183cb59bbe4ef0a235a3c21ca52b9e26
Diogo 'Mozão' Fonseca <devmozao@outlook.com>
2020-02-29 21:48:27 -0300

    parser completed and working

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 'use strict'
 
-var fs = require('fs')
-var parser = require('xml2json')
+const fs = require('fs')
+const br = "\n" // line breaker
 
-const document = fs.readFile('./file.xml', (error, data) => {
-  return JSON.parse(parser.toJson(data, { reversible: true }))
+function parseToJSON(data) {
+  return JSON.stringify(data)
+}
+
+function replacer(data) {
+  return data
+    .replace(/&amp;/g, '&')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&lt;/g, '<')
+    .replace(/&apos/g, "'")
+}
+
+// reads the file and stores in variable. we must encode to utf-8 to not get unformatted data, then log the result
+const document = fs.readFileSync('./file.xml', 'utf8', (error, data) => {
+  return data
 })
+console.log(br, 'document state after loading and encoding', br, document)
 
-console.dir('file', document)
+// here we first parse to JSON and then replace the values globally with regex/string substitution, then log the result
+const documentParsedAndReplaced = replacer(parseToJSON(document))
+console.log(br, 'result after passing to json and replacing all', br, br, documentParsedAndReplaced)
+
+// here we directly replace the raw document with with regex/string, then log the result
+const rawDocumentReplaced = replacer(document)
+console.log(br, 'result after replacing the raw xml document', br, rawDocumentReplaced)
